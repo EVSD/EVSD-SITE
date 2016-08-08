720ff9a5257f9f484e393fb0125cba76d9a05c9e
Michelle Li <michellecarolynclark@gmail.com>
2016-08-07 21:59:54 -0700

    fixed key reference
    security later

--- client/api/payments/signupPayment.js
+++ client/api/payments/signupPayment.js
@@ -3,7 +3,9 @@ if (Meteor.isClient) {
     'click button': function(e) {
       e.preventDefault();
       StripeCheckout.open({
-        key: Meteor.settings.public.stripe.testPublishableKey,
+        // change this to the Meteor.settings.public.stripe.testPublishableKey later
+        // right now it doesn't work for some reason
+        key: "pk_test_0aiE07z5Mf1yDmYgZIu3RY7K",
         amount: 25000, // this is equivalent to $25000
         name: 'EVSD Contribution',
         //description: 'As reviewed in the Parent Orientation, this is the bare minimum we need to cover coaching expenses and salaries, facilities, school tournament fees, club events, financial aid & subsidies to students, league fees, professional material, and much more. Most schools ask for around $400-$600, so we are trying our best to do with as little aid as possible. Please contact team administration at evhs.sd@gmail.com for questions or concerns. We use Paypal and Stripe software to power our payment process, which means our platform is verified and 100% safe. Because we are a registered 501c3 under California State Government and an ESUHSD Booster, your contribution is tax-deductible.',
