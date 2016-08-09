afe14df26eb48db79cbe64913f05078a08030107
Michelle Li <michellecarolynclark@gmail.com>
2016-08-08 18:01:09 -0700

    changed payment method to Meteor.apply to prevent multiple client calls if disconnect

--- client/api/payments/signupPayment.js
+++ client/api/payments/signupPayment.js
@@ -11,10 +11,12 @@ if (Meteor.isClient) {
         //description: 'As reviewed in the Parent Orientation, this is the bare minimum we need to cover coaching expenses and salaries, facilities, school tournament fees, club events, financial aid & subsidies to students, league fees, professional material, and much more. Most schools ask for around $400-$600, so we are trying our best to do with as little aid as possible. Please contact team administration at evhs.sd@gmail.com for questions or concerns. We use Paypal and Stripe software to power our payment process, which means our platform is verified and 100% safe. Because we are a registered 501c3 under California State Government and an ESUHSD Booster, your contribution is tax-deductible.',
         description: 'This covers all associated fees.',
         panelLabel: 'Pay Now',
-        token: function(res) {
-          stripeToken = res.id;
-          console.info(res);
-          Meteor.call('chargeCard', stripeToken);
+        token: function(response) {
+          stripeToken = response.id;
+          //console.info('response: ' + response);
+          //Meteor.call('chargeCard', stripeToken);
+          // prevents multiple charges if client disconnects and reconnects
+          Meteor.apply('chargeCard', [stripeToken], {noRetry: true});
         }
       });
     }
