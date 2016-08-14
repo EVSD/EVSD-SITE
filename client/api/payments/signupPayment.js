4de035c4411d4b057e8c4a6eabc90c832e9dd50b
Michelle Li <michellecarolynclark@gmail.com>
2016-08-13 23:02:22 -0700

    removed duplicate method chargeCard from inaccessible location

--- client/api/payments/signupPayment.js
+++ client/api/payments/signupPayment.js
@@ -20,19 +20,3 @@ if (Meteor.isClient) {
     }
   });
 }
-
-if (Meteor.isServer) {
-  Meteor.methods({
-    'chargeCard': function(stripeToken) {
-      check(stripeToken, String);
-      var Stripe = StripeAPI(Meteor.settings.private.stripe.testSecretKey);
-      Stripe.charges.create({
-        source: stripeToken,
-        amount: 25000, // this is equivalent to $250
-        currency: 'usd'
-      }, function(err, charge) {
-        console.log(err, charge);
-      });
-    }
-  });
-}
