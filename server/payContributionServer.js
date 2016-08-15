644c96df77439cdf45fbe6ca5e4e96eb903ef8e9
Jackson <jacksonchui.qwerty@gmail.com>
2016-08-14 18:30:49 -0700

    Update payContributionServer.js
    email change #1 to solve issue

--- server/payContributionServer.js
+++ server/payContributionServer.js
@@ -8,11 +8,7 @@
         source: stripeToken,
         amount: 25000, // this is equivalent to $250
         currency: 'usd',
-<<<<<<< HEAD
         receipt_email: userEmail
-=======
-        // TODO: add receipt_email: 
->>>>>>> f7696b0fdf422196891921b6ee58302f368d7032
       }, function(err, charge) {
         console.log(err, charge);
         if (charge.status == 'succeeded') {
