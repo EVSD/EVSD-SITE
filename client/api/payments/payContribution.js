778c7a38b5baecee4cb591fbc68882fea3ce567a
Michelle Li <michellecarolynclark@gmail.com>
2016-08-14 19:03:10 -0700

    fixed payment email

--- client/api/payments/payContribution.js
+++ client/api/payments/payContribution.js
@@ -9,15 +9,15 @@ if (Meteor.isClient) {
         // description: 'As reviewed in the Parent Orientation, this is the bare minimum we need to cover coaching expenses and salaries, facilities, school tournament fees, club events, financial aid & subsidies to students, league fees, professional material, and much more. Most schools ask for around $400-$600, so we are trying our best to do with as little aid as possible. Please contact team administration at evhs.sd@gmail.com for questions or concerns. We use Paypal and Stripe software to power our payment process, which means our platform is verified and 100% safe. Because we are a registered 501c3 under California State Government and an ESUHSD Booster, your contribution is tax-deductible.',
         description: 'This covers all associated fees.',
         panelLabel: 'Pay Now',
-        //get user email        
+        //get user email
         token: function(response) {
           stripeToken = response.id;
-          let userEmail = Meteor.user().emails[0].address;
-          console.log("userEmail");
+          // let userEmail = Meteor.user().emails[0].address;
+          //console.log("userEmail");
           // console.info('response: ' + response);
           // Meteor.call('chargeCard', stripeToken);
           // prevents multiple charges if client disconnects and reconnects
-          Meteor.apply('chargeCard', [stripeToken], userEmail, {noRetry: true});
+          Meteor.apply('chargeCard', [stripeToken], {noRetry: true});
         }
       });
     }
