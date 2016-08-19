d7a7ecfd8f8bb13cadc37abdf0c0bb806b8c0e96
Michelle Li <michellecarolynclark@gmail.com>
2016-08-19 08:09:02 -0700

    fixed account balance

--- client/api/accountBalance.js
+++
@@ -1,9 +0,0 @@
-Template.accountBalance.returnUserBalance = function(){
-  if (Meteor.user())
-    return Meteor.user().profile.accountBalanceLog;
-};
-
-Template.accountBalance.helpers({
-  accountBalanceLog: Template.accountBalance.returnUserBalance()
-  //accountBalanceLog: [{name: 'hi', amount: 100, date: 'hi'}]
-});
