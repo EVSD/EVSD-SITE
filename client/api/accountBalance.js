44d829e365530715c6eca1bf9165a72074e9b844
Michelle Li <michellecarolynclark@gmail.com>
2016-08-18 21:29:49 -0700

    better code but still doesnt work

--- client/api/accountBalance.js
+++ client/api/accountBalance.js
@@ -1,14 +1,9 @@
-/*Template.accountBalance.returnUserBalance = function(){
+Template.accountBalance.returnUserBalance = function(){
   if (Meteor.user())
     return Meteor.user().profile.accountBalanceLog;
-};*/ //thisworks
+};
 
 Template.accountBalance.helpers({
-  returnUserBalance(){
-    if (Meteor.user())
-      accountBalanceLog: Meteor.user().profile.accountBalanceLog;
-  }
-  // this returns an error: Blaze._globalHelpers.returnUserBalance is not a function
-  //accountBalanceLog: Blaze._globalHelpers.returnUserBalance()
+  accountBalanceLog: Template.accountBalance.returnUserBalance()
   //accountBalanceLog: [{name: 'hi', amount: 100, date: 'hi'}]
 });
