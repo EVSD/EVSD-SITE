d7cc5b7f4766b09f683ad8b6b8c8e482e6d9309e
Michelle Li <michellecarolynclark@gmail.com>
2016-08-29 19:02:04 -0700

    fixed change user balance bug

--- client/api/payments/changeUserBalance.js
+++ client/api/payments/changeUserBalance.js
@@ -7,13 +7,11 @@ if (Meteor.isClient){
 	Template.changeUserBalance.events({
 		'submit .change-balance':function(event){
 			event.preventDefault();
+			let username = event.target.user.value;
       let eventName = event.target.name.value;
 			let eventAmount = event.target.amount.value;
 			let eventDescription = event.target.description.value;
-      Meteor.users.update(Meteor.userId(), {
-        $set: {"profile.balance": (Meteor.user().profile.balance + Number(eventAmount))}
       })
-			Meteor.users.update(Meteor.userId(), {
 				$addToSet: {"profile.accountBalanceLog": {cc: true, description: eventDescription, checkNo: 0, paymentMethod: "administrative manual payment logging", amount: eventAmount, name: eventName, date: new Date(), dateWritten: new Date(), dateDeposited: new Date(), memo: ""}}
 			})
 		},
