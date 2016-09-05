dd080a24e138dee577f8aad9eb10dc8e0e6f168c
Jackson Chui <jacksonchui.qwerty@gmail.com>
2016-09-05 10:13:37 -0700

    fixed slingshot

--- client/api/upload/uploader.js
+++ client/api/upload/uploader.js
@@ -2,14 +2,15 @@ Template.uploader.events({
   'change input[type="file"]' ( event, template ) {
     if (confirm("Are you sure that you want to upload this file?")){
       Modules.client.uploadWaiversToAmazonS3( { event: event, template: template } );
-      }else{
-        Bert.alert("file did not upload");
-      }
+      console.log(waiverUrl);
       Meteor.call('editProfileFileUrl', waiverUrl, function(err) {
         if (err){
           console.log('could not append file');
           Bert.alert('reg user error','danger');
         }
       });
+      }else{
+        Bert.alert("file did not upload");
+      }
   }//end of change
 });
