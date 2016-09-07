be04ac49c30b6ad27cec1e6aa077ddc871eb9a1f
Michelle Li <michellecarolynclark@gmail.com>
2016-09-06 21:50:28 -0700

    logic fix

--- client/api/login/login.js
+++ client/api/login/login.js
@@ -9,7 +9,7 @@ if (Meteor.isClient){
 	            	if(err){
 	                	alert(err);
 	            	}else{
-									if (Meteor.user().profile.payContribution == "no" || Meteor.user().profile.paidContribution == "no") FlowRouter.go('/payContribution');
+									if (Meteor.user().profile.payContribution == "no" && Meteor.user().profile.paidContribution == "no") FlowRouter.go('/payContribution');
 									else if (Meteor.user().profile.waiver == "no") FlowRouter.go('/waiver');
 									else FlowRouter.go('/profile');
 								}
