1fc4530bc1414d6e3a8298759142eb4940431d16
Richard Huang <richardhuang.huang@gmail.com>
2016-09-24 14:24:29 -0700

    complete formatting

--- server/smtp.js
+++ server/smtp.js
@@ -10,4 +10,10 @@ Meteor.startup(function () {
 
   Accounts.emailTemplates.siteName = "EVSD";
 	Accounts.emailTemplates.from = "EVSD <evhs.sd@gmail.com>";
+  Accounts.emailTemplates.resetPassword.subject= function(user){
+    return "Reset EVSD Password";
+  };
+  Accounts.emailTemplates.resetPassword.text= function(user,url){
+    return "Click this link to reset your password:\nhttp://evsd.club/#/reset-password/"+url.substring(url.lastIndexOf('/') + 1);
+  };
 });
