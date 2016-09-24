e8f8bdfd8d741c342bc847f0c600e6be0d62dcdb
Richard Huang <richardhuang.huang@gmail.com>
2016-09-24 13:42:46 -0700

    setting up smtp

---
+++ server/smtp.js
@@ -0,0 +1,19 @@
+Meteor.startup(function () {
+  smtp = {
+    username: Meteor.settings.MAIL_USER,   // eg: server@gentlenode.com
+    password: Meteor.settings.MAIL_PASS,   // eg: 3eeP1gtizk5eziohfervU
+    server:   'smtp.gmail.com',  // eg: mail.gandi.net
+    port: 587 //25
+  }
+
+  process.env.MAIL_URL = 'smtp://' + encodeURIComponent(smtp.username) + ':' + encodeURIComponent(smtp.password) + '@' + encodeURIComponent(smtp.server) + ':' + smtp.port;
+
+  Accounts.emailTemplates.siteName = "EVSD";
+	Accounts.emailTemplates.from = "EVSD <evhs.sd@gmail.com>";
+	Accounts.emailTemplates.verifyEmail.subject  = function (user) {
+		return "Reset Password";
+		};
+	Accounts.emailTemplates.verifyEmail.text = function (user, url) {
+		return  "Test";
+		};
+});
