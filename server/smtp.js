af5adb2b6ac46c4341f0080ef83841b930fc55ae
Richard Huang <richardhuang.huang@gmail.com>
2016-10-01 06:29:55 +0000

    updates

--- server/smtp.js
+++ server/smtp.js
@@ -3,7 +3,7 @@ Meteor.startup(function () {
     username: Meteor.settings.MAIL_USER,   // eg: server@gentlenode.com
     password: Meteor.settings.MAIL_PASS,   // eg: 3eeP1gtizk5eziohfervU
     server:   'smtp.gmail.com',  // eg: mail.gandi.net
-    port: 587 //25
+    port: 587 //25 old; 465 sl; 587 non-ssl
   }
 
   process.env.MAIL_URL = 'smtp://' + encodeURIComponent(smtp.username) + ':' + encodeURIComponent(smtp.password) + '@' + encodeURIComponent(smtp.server) + ':' + smtp.port;
