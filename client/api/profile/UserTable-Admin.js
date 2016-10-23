4c9c8be1b91576f69470cb337f76de1c25132eea
Michelle Li <michellecarolynclark@gmail.com>
2016-10-23 11:04:09 -0700

    admin can remove users

--- client/api/profile/UserTable-Admin.js
+++ client/api/profile/UserTable-Admin.js
@@ -1,28 +1,37 @@
 if (Meteor.isClient){
 	Template.userTable.helpers({
-	users: function () {
-		return Meteor.users.find({}, {
-			sort: { "profile.lastName": 1 }
-		});
-	}
-});
+		users: function () {
+			return Meteor.users.find({}, {
+				sort: { "profile.lastName": 1 }
+			});
+		}
+	});
 	Template.userTable.events({
-	   	//change user roles
+		//change user roles
 		'change [name="userRole"]': function( event) {
 			if (confirm('Are you sure?')){
 				let role = $( event.target ).find( 'option:selected' ).val();
-			    //console.log(role);
+				//console.log(role);
 				if (role != 'admin'){
-				    Meteor.call( "setRoleOnUser", {
-				      user: this._id,
-				      role: role
-				    }, ( error, response ) => {
-				      if ( error ) {
-				        Bert.alert( error.reason, "warning" );
-				      }
-				    	});
+					Meteor.call( "setRoleOnUser", {
+						user: this._id,
+						role: role
+					}, ( error, response ) => {
+						if ( error ) {
+							Bert.alert( error.reason, "warning" );
+						}
+					});
 				} else Bert.alert ("You cannot make this user an admin");
 			} else return false;
+		},
+		'click .remove-user': function(event) {
+			Meteor.users.remove({ _id: this._id }, function (error, result) {
+				if (error) {
+					Bert.alert("Error removing user: " + error);
+				} else {
+					Bert.alert("Successfully removed user", "success");
+				}
+			})
 		},
 	});
 }
--- server/allow.js
+++ server/allow.js
@@ -2,5 +2,8 @@ Meteor.users.allow({
   update: function(userId, user) {
     return Roles.userIsInRole( userId, 'admin' );
     return Roles.userIsInRole(userId, 'officer');
+  },
+  remove: function(userId) {
+    return Roles.userIsInRole( userId, 'admin' );
   }
 });
