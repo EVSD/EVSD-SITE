25d91056be104eddedf6f33e7daaec1f7436bda8
Richard Huang <richardhuang.huang@gmail.com>
2017-10-08 09:10:35 -0700

    generalized sign up button

--- client/resetPassword.js
+++ client/resetPassword.js
@@ -30,34 +30,34 @@ Template.ForgotPassword.events({
 });
 
 
-// if (Accounts._resetPasswordToken) {
-//   Session.set('resetPassword', Accounts._resetPasswordToken);
-// }
-//
-// Template.ResetPassword.helpers({
-//  resetPassword: function(){
-//   return Session.get('resetPassword');
-//  }
-// });
-//
-// Template.ResetPassword.events({
-//   'submit #resetPasswordForm': function(e, t) {
-//     e.preventDefault();
-//
-//     var resetPasswordForm = $(e.currentTarget),
-//         password = resetPasswordForm.find('#resetPasswordPassword').val(),
-//         passwordConfirm = resetPasswordForm.find('#resetPasswordPasswordConfirm').val();
-//
-//     // if (isNotEmpty(password) && areValidPasswords(password, passwordConfirm)) {
-//       Accounts.resetPassword(Session.get('resetPassword'), password, function(err) {
-//         if (err) {
-//           Bert.alert('We are sorry but something went wrong.','danger','growl-top-right');
-//         } else {
-//           Bert.alert('Your password has been changed. Welcome back!','danger','growl-top-right');
-//           Session.set('resetPassword', null);
-//         }
-//       });
-//     // }
-//     return false;
-//   }
-// });
+if (Accounts._resetPasswordToken) {
+  Session.set('resetPassword', Accounts._resetPasswordToken);
+}
+
+Template.ResetPassword.helpers({
+ resetPassword: function(){
+  return Session.get('resetPassword');
+ }
+});
+
+Template.ResetPassword.events({
+  'submit #resetPasswordForm': function(e, t) {
+    e.preventDefault();
+
+    var resetPasswordForm = $(e.currentTarget),
+        password = resetPasswordForm.find('#resetPasswordPassword').val(),
+        passwordConfirm = resetPasswordForm.find('#resetPasswordPasswordConfirm').val();
+
+    // if (isNotEmpty(password) && areValidPasswords(password, passwordConfirm)) {
+      Accounts.resetPassword(Session.get('resetPassword'), password, function(err) {
+        if (err) {
+          Bert.alert('We are sorry but something went wrong.','danger','growl-top-right');
+        } else {
+          Bert.alert('Your password has been changed. Welcome back!','danger','growl-top-right');
+          Session.set('resetPassword', null);
+        }
+      });
+    // }
+    return false;
+  }
+});
